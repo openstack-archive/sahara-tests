b25514bccb90478f4824a969fb66b2ec151b2744
Alina Nesterova <zemuvier@gmail.com>
2016-07-25 15:45:33 +0300

    Added cli-tests for job template
    These tests include creating, updating, showing and deleting
    of job template and also list of all them
    
    Change-Id: I0a5d89092b1226364d80997203a4ba86848a41b6

--- sahara_tempest_plugin/tests/cli/job_templates.py
+++ sahara_tempest_plugin/tests/cli/job_templates.py
@@ -10,6 +10,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from tempest.lib.common.utils import data_utils
+
 from sahara_tempest_plugin.tests.cli import base
 
 
@@ -21,3 +23,33 @@ class SaharaJobTemplateCLITest(base.ClientTestBase):
             'Id',
             'Type'
         ])
+
+    def openstack_job_template_create(self, job_binary_name):
+        job_template_name = data_utils.rand_name('job-template')
+        flag = ("%(name)s %(type)s %(main)s "
+                % {'name': ' --name %s' % job_template_name,
+                   'type': ' --type Shell',
+                   'main': ' --main %s' % job_binary_name})
+        self.assertTableStruct(
+            self.listing_result('job template create %s' % flag),
+            [
+                'Field',
+                'Value'
+            ])
+        return job_template_name
+
+    def openstack_job_template_show(self, job_template_name):
+        self.find_in_listing(
+            self.listing_result('job template show %s' % job_template_name),
+            job_template_name)
+
+    def openstack_job_template_update(self, job_template_name):
+        self.assertTableStruct(
+            self.listing_result('job template update --description '
+                                'cli-tests %s' % job_template_name), [
+                'Field',
+                'Value'
+            ])
+
+    def openstack_job_template_delete(self, job_template_name):
+        self.check_if_delete('job template', job_template_name)
